6f731156231219bd8ea553ad7390b500a8b15036
axton21 <axton_n@yahoo.com>
2015-05-10 14:14:18 -0400

    refactored

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,7 +1,6 @@
 require 'pry'
 def power_digit_sum(num, exponent)
-	total = num ** exponent
 	number = 0
-	total.to_s.split("").each{|x|number += x.to_i}
+	(num ** exponent).to_s.split("").each{|x|number += x.to_i}
 	number
 end
